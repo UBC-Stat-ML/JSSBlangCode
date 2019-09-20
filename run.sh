3bbbc2623fbee515432bc593a8ee4bf62786bbf2
Sahand-H <sahandhosseinii@gmail.com>
2019-09-19 22:31:11 -0700

    add script to run all models

---
+++ run.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Requires a UNIX-compatible environment running bash, and git software.
+
+# mkdir RUN_DIR
+# cd RUN_DIR
+
+echo "Setting up the CLI"
+read -n 1 -s -r -p "Press any key to continue."
+echo
+# Unpack SDK and setup the CLI
+# cd ..
+tar -xzvf ../blangSDK-1.33.2.tar.gz
+cd blangSDK-1.33.2
+echo
+source setup-cli.sh
+echo
+cd ../example
+
+# Run the Doomsday example
+echo "Running Doomsday"
+read -n 1 -s -r -p "Press any key to continue."
+echo
+cd example
+blang --model jss.Doomsday --model.rate 1.0 --model.y 1.2 --model.z NA
+
+
+echo "Running PhylogeneticTree"
+read -n 1 -s -r -p "Press any key to continue."
+echo
+blang --model jss.phylo.PhylogeneticTree \
+   --model.observations.file data/primates.fasta \
+   --model.observations.encoding DNA \
+   --engine PT \
+   --engine.random 1
+
+
+echo "Running SpikeSlabClassification"
+read -n 1 -s -r -p "Press any key to continue."
+echo
+blang --model jss.glms.SpikeSlabClassification \
+   --model.data data/titanic/titanic-covariates.csv \
+   --model.instances.name Name \
+   --model.instances.maxSize 200 \
+   --model.labels.dataSource data/titanic/titanic.csv \
+   --model.labels.name Survived \
+   --engine PT \
+   --engine.nChains 20 \
+   --postProcessor DefaultPostProcessor
+
+
+echo "Running Rocket"
+read -n 1 -s -r -p "Press any key to continue."
+echo
+blang --model jss.hier.Rocket --model.data data/rockets.csv --model.countries.name Country --model.rockets.name Rockets --model.nLaunches.name nLaunches --model.nFails.name nFails
+
+echo
+echo
+
+echo "================================================="
+echo "Now swapping to run models in subdirectory others "
+echo "================================================="
+read -n 1 -s -r -p "Press any key to continue."
+
+cd ../others
+
+echo
+
+echo "================================================="
+echo "Setting up gradle project"
+echo "================================================="
+echo
+./gradlew clean
+./gradlew installDist
+
+
+models=(uniform exponential ising markov-chain plated-matrix poisson-mixture permutation)
+
+echo
+echo
+
+echo "================================================="
+echo "================================================="
+echo
+echo " Available models are will be listed below. Use  "
+echo " command: 'build/install/others/bin/others name  "
+echo
+echo "================================================="
+echo "================================================="
+read -n 1 -s -r -p "Press any key to continue."
+
+echo
+for item in ${models[@]}
+do
+  echo
+  echo
+  echo "================================================="
+  echo "Running Model " $item
+  echo "================================================="
+  read -n 1 -s -r -p "Press any key to continue."
+  echo
+  build/install/others/bin/others $item
+done
+
+echo "All models ran successfully."
+
+echo
+echo
+
+echo "================================================="
+echo "Now running:"
+echo
+echo "1. ExactInvarianceTestComplicatedModel.xtend"
+echo "2. TestComplicatedModel.xtend "
+echo "================================================="
+read -n 1 -s -r -p "Press any key to continue."
+echo
+
+echo
+./gradlew test
